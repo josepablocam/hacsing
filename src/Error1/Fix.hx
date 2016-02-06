039d84f3035ffd4a166fd4d171fe7e9b313d74f4
Jose Cambronero <jpc485@energon1.cims.nyu.edu>
2016-02-06 12:10:55 -0500

    renamed files, and ironically enough fixed sort issue in comments

---
+++ src/Error1/Fix.hx
@@ -0,0 +1,26 @@
+/*
+Scheme signatures are contracts to be fulfilled appropriately.
+If you state a scheme takes a given sort, and then had it something
+different, you'll be in trouble.
+
+Recreate by calling
+./Error1 --sort=Num --scheme=Test --term="123"
+
+*/
+
+
+module com.github.hacsing.Fix1 {
+
+ token NUM  | [0-9]+;
+ token WORD | [a-zA-Z]+;
+ sort Num   | ⟦⟨NUM⟩⟧;
+ sort Word  | ⟦⟨WORD⟩⟧;
+ 
+ sort Program | ⟦⟨Num⟩⟧ | ⟦⟨Word⟩⟧;
+
+ sort Program | scheme Test(Num);
+ Test(#) → Test2(#);
+
+ sort Program | scheme Test2(Num); // FIX
+ Test2(#) → ⟦⟨Num#⟩⟧;
+}
